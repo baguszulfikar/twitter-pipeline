e1e18689149e0fbf2a041fa81131810c84a5866a
Bagus Zulfikar <50280372+baguszulfikar@users.noreply.github.com>
2022-07-03 11:29:27 +0700

    Delete config.py

--- config.py
+++
@@ -1,4 +0,0 @@
-consumer_key = "0GHBBLM1BImXSmUVI5hQP1bou"
-consumer_secret = "a5OxetJT9q2jJFD5E8Jo0X8YGe88vgj0MaxLnJUc8tzA5jgtBp"
-access_token = "61354924-E7l18ch4TOtARLGBAEJqRjg6SnM0mXOEAlEh4tEdX"
-access_token_secret = "P5P096kCzSzX3Kab4uHxULVar4Jfpmi9oKHQjPOyKaEhq"
